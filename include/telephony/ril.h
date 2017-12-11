08fdde5198be3103b61a263a4ab75018c2a0d21e
shivatejapeddi <shivatejapeddi@gmail.com>
2017-12-11 07:28:25 +0000

    fixes for volte

--- include/telephony/ril.h
+++ include/telephony/ril.h
@@ -602,7 +602,7 @@ typedef struct {
 } RIL_Dial;
 
 typedef struct {
-    int cla;        /* OnePlus */
+    int cla;
     int command;    /* one of the commands listed for TS 27.007 +CRSM*/
     int fileid;     /* EF id */
     char *path;     /* "pathid" from TS 27.007 +CRSM command.
@@ -617,7 +617,7 @@ typedef struct {
 } RIL_SIM_IO_v5;
 
 typedef struct {
-    int cla;        /* OnePlus */
+    int cla;
     int command;    /* one of the commands listed for TS 27.007 +CRSM*/
     int fileid;     /* EF id */
     char *path;     /* "pathid" from TS 27.007 +CRSM command.
